5fa002db05d5395494cd2b8c35a764fe606662a0
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2021-07-24 10:58:20 +0300

    119. Работа с формами отправки данных

---
+++ src/js/modules/forms.js
@@ -0,0 +1,53 @@
+const forms = () => {
+
+    const form = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+    const message = {
+        loading: 'loading',
+        success: 'success',
+        failture: 'failture'
+    };
+
+    const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        const res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(input => input.value='');
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+            const formData = new FormData(item);
+            postData('assets/server.php', formData)
+                .then(res => {
+                    document.querySelector('.status').textContent = message.success;
+                    console.log(res);
+                })
+                .catch(() => document.querySelector('.status').textContent = message.failture)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => statusMessage.remove(), 5000);
+                });
+        });
+    });
+
+};
+
+export default forms;
