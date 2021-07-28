6b77ea010242ce2dcec074b43d0d27b343cecde1
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2021-07-28 22:56:25 +0300

    124. Улучшаем наш проект (анимации, правильное

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,7 +5,8 @@ const modals = (state) => {
         const   triggers = document.querySelectorAll(triggerSelector),
                 modal = document.querySelector(modalSelector),
                 close = document.querySelector(closeSelector),
-                windows = document.querySelectorAll('[data-modal]');
+                windows = document.querySelectorAll('[data-modal]'),
+                scroll = calcScroll();
 
         triggers.forEach(element => {
             element.addEventListener('click', (e) => {
@@ -46,11 +47,13 @@ const modals = (state) => {
         const openModal = () => {
             modal.style.display = 'block';
             document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
             //document.body.classList.add('modal-open');
         };
         const closeModal = () => {
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
             //document.body.classList.remove('modal-open');
         };
 
@@ -61,6 +64,18 @@ const modals = (state) => {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = 'hidden';
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
